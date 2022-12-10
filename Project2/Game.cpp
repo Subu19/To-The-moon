a703a5c5b4db7000d71d9933c03ff8b4aeff1ccb
Subu <59548115+Subu19@users.noreply.github.com>
2022-12-10 18:50:07 +0545

    First Commit

--- Project2/Game.cpp
+++ Project2/Game.cpp
@@ -620,6 +620,7 @@ void Game::updateEnemies() {
                             }
                     } 
         }
+
     }
     else if(firstStageCompleted == true && portalEffect == true) {
         
